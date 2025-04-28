59a6620ae1df5ff92a634f8df08739f6b9c61bd1
failququ <failququmoar@gmail.com>
2025-04-28 09:31:19 +0300

    fix pages stories with provider

--- src/pages/NotFoundPage/ui/NotFoundPage.stories.tsx
+++ src/pages/NotFoundPage/ui/NotFoundPage.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { Theme } from 'app/providers/ThemeProvider/lib/ThemeContext';
+import { StoreDecorator } from 'shared/config/storybook/decorators';
 import { ThemeDecorator } from 'shared/config/storybook/decorators/ThemeDecorator/ThemeDecorator';
 import NotFoundPage from './NotFoundPage';
 
@@ -13,6 +14,7 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {};
+Default.decorators = [StoreDecorator({})];
 
 export const Dark: Story = {};
-Dark.decorators = [ThemeDecorator(Theme.Dark)];
+Dark.decorators = [ThemeDecorator(Theme.Dark), StoreDecorator({})];
