87519caa352631259aa04cde033dd875dfd7ac4f
failququ <failququmoar@gmail.com>
2025-05-14 08:49:34 +0300

    add jest html reporter

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -54,6 +54,14 @@ const config: Config = {
     __API__: '',
     __PROJECT__: 'jest',
   },
+  reporters: [
+    'default',
+    ['jest-html-reporters', {
+      publicPath: '<rootDir>/reports/unit',
+      filename: 'report.html',
+      openReport: true,
+    }],
+  ],
   // Indicates whether the coverage information should be collected while executing the test
   // collectCoverage: false,
 
